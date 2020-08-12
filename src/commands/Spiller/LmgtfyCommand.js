07c79a2ceae8d774dae16f0f31cc5f2bfb765c72
Moritz Myrseth <moritzmyrz@gmail.com>
2020-08-12 21:26:25 +0000

    ✔ Fiksa SyntaxError for LMGTFY

--- src/commands/Spiller/LmgtfyCommand.js
+++ src/commands/Spiller/LmgtfyCommand.js
@@ -7,6 +7,7 @@ module.exports = class LmgtfyCommand extends BaseCommand {
 
   run(client, message, args) {
     const content = message.content.slice(8).split(' ').join('+');
+    if (!args[0]) return message.channel.send(`Bruk kommandoen følgende: \`\`!lmgtfy <søk>\`\``);
     message.channel.send(`https://lmgtfy.com/?q=${content}&iie=1`);
   }
 }
